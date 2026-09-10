61c390c21c8b4379e7d7759b757abcc7b6b66dde
BazhenovYura <133841115+BazhenovYura@users.noreply.github.com>
2026-09-10 12:03:17 +0500

    Update Types.tsx

--- src/sections/Types.tsx
+++ src/sections/Types.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import { X, Upload } from 'lucide-react';
 import { sendMetrikaGoal, sendMetrikaEvent } from '../utils/metrika';
 
