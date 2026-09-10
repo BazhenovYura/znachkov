e5f3d14f758f8a85da84cef0c6964a0049b3f544
BazhenovYura <133841115+BazhenovYura@users.noreply.github.com>
2026-09-10 12:10:32 +0500

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import { ArrowRight, Calendar, Clock, Shield, Truck, X, Upload } from 'lucide-react';
 import { sendMetrikaGoal, sendMetrikaEvent } from '../utils/metrika';
 
