fbd396e651c22b5cd0911f2716854e8f907320d5
BazhenovYura <133841115+BazhenovYura@users.noreply.github.com>
2026-09-10 17:52:32 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
+
 
 let wasmPlugin: any = null;
 let topLevelAwaitPlugin: any = null;
@@ -22,8 +22,7 @@ try {
 
 export default defineConfig({
   base: './',
-  plugins: [
-    inspectAttr(), 
+  plugins: [    
     react(),
     wasmPlugin && wasmPlugin(),
     topLevelAwaitPlugin && topLevelAwaitPlugin(),
