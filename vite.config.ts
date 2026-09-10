6d4937341f8147e8b77a2757d25f4a4cebcad4eb
BazhenovYura <133841115+BazhenovYura@users.noreply.github.com>
2026-09-10 13:04:41 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
       'react',
       'react-dom',
       'react-router',
-      'react-router-dom',
+      'react-icons',
       'cookie',
       'lucide-react',
     ],
