829f4148a8deec207a415f908234f66714e81bbc
BazhenovYura <133841115+BazhenovYura@users.noreply.github.com>
2026-09-10 15:28:21 +0500

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, useLocation } from 'react-router';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { Phone, Mail, MapPin } from 'lucide-react';
 import { sendMetrikaGoal, sendMetrikaEvent } from '../utils/metrika';
 
